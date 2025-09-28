3f7da26674c4bac685e48b96e92767673cf8d7e7
Socrates0007 <69106577+Socrates0007@users.noreply.github.com>
2025-09-28 11:03:44 +0100

    first commit without states

---
+++ personal_sheets.py
@@ -0,0 +1,33 @@
+
+# personal_sheets.py
+# Each personal sheet MUST share the same header as in config.HEADERS.
+
+PEOPLE = [
+    {
+            "name": "Grace",
+            "sheet_id": "1DLtMRRZC0BMjvQbLrtty7-vrTjtzRsjLNhpsI_RbEWY",
+            #"whatsapp": "+2348071669947"
+            "whatsapp": "+2347038599873"
+           ,
+                                },
+                          
+    {
+        "name": "Joy",
+        "sheet_id": "1UBTFv_XqnA-zUtnpQoWE-naYkayR3ncgqgo30QJgvgs",
+        "whatsapp": "+2349036090803",
+    },
+    {
+        "name": "Faith",
+        "sheet_id": "1NTdFFd9tptK6gNgEN_OU44Q4lNQk781MMdlpU5empNo",
+        "whatsapp": "+2349076863604",
+    },
+    {
+        "name": "Monica",
+        "sheet_id": "1VWGq-FlEr2ZJb7rgVhaVlExQohueoK-6q5qqRnUdaeM",
+        "whatsapp": "+2348030467376",
+    }
+
+    ]
+
+if __name__ == '__main__':
+    print(PEOPLE)
