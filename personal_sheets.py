a0604f647a0e790dab1822bf5fe081320b3b51eb
Socrates0007 <69106577+Socrates0007@users.noreply.github.com>
2026-07-01 05:26:30 +0000

    July setup

--- personal_sheets.py
+++ personal_sheets.py
@@ -6,7 +6,7 @@ PEOPLE = [
     {
             "name": "Grace",
             "sheet_id": "1DLtMRRZC0BMjvQbLrtty7-vrTjtzRsjLNhpsI_RbEWY",
-            "whatsapp": "+2348071669947"
+            "whatsapp": "+2348103685623"
             #"whatsapp": "+2347038599873"
            ,
                                 },
@@ -14,15 +14,11 @@ PEOPLE = [
     {
         "name": "Joy",
         "sheet_id": "1SN8mK4y6hha1wkWmPo2puS0KrkuFdfRK9LZIik0oi_E",
-        "whatsapp": "+2349036090803"
+        "whatsapp": "+2348102534494"
         #"whatsapp": "+2347038599873"
         ,
     },
-    {
-        "name": "Faith",
-        "sheet_id": "1NTdFFd9tptK6gNgEN_OU44Q4lNQk781MMdlpU5empNo",
-        "whatsapp": "+2349076863604",
-    },
+   
     {
         "name": "Monica",
         "sheet_id": "1VWGq-FlEr2ZJb7rgVhaVlExQohueoK-6q5qqRnUdaeM",
